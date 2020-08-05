ea56ebdcb8ac22ad1768c8a8ffd603e9921d5af0
Sam Zyra <sisaaczyra@gmail.com>
2020-08-04 19:50:19 -0700

    Final updates. Fixed existing issues and completed all requirements.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This app will allow the user to take a timed multiple-choice quiz on simple codi
 The user will earn one point for each correct answer. They will lose five seconds from the testing time for each incorrect answer. 
 If they answer all the questions or run out of time, the app will allow them to record their score with their initials. They may take it again to try and beat their score.
 
+Updates:
 Workday 1: 
 Arranged basic formatting and initial working pieces. Installed a start button that will disappear when clicked, allowing new visuals to take its place.
 
@@ -25,3 +26,6 @@ Time is penalized for every incorrect answer correctly.
 
 	To-do:
 	- Ensure recorded answers are stored locally
+
+Workday 4:
+Fixed issue with final screen and with timer glitch. Ensured all answers were stored locally.
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var quizArea = document.querySelector('#questionSpot');
 var begin = document.querySelector('#button');
 var initialArea = document.getElementById('record');
+var initials = document.getElementById('initials');
+var initialForm = document.getElementById('form');
+var allScores = document.getElementById('scores');
 
 var question = document.querySelector('#questionAsk');
 var firstOp = document.querySelector('#optionA');
@@ -10,8 +13,14 @@ var thirdOp = document.querySelector('#optionC');
 var scoreCount = document.getElementById('score');
 var scorePlus = 0;
 var timer = document.getElementById('time');
+var timeInterval;
 var seconds = 60;
 
+function endTest() {
+    clearInterval(timeInterval);
+    quizArea.style.visibility="hidden";
+    initialArea.style.visibility="visible";
+}
 
 var allQuestions = [
     {q:"Q1: What does HTML stand for?", a:"Hyperlinks and Text Markup Language", b:"Home Tool Markup Language", c:"Hyper Text Markup Language", answer:"c"},
@@ -42,33 +51,37 @@ function buttonpress() {
 
 
 // The test displays each question and its corresponding options in order
-function actualTest() {
+function actualTest(i) {
     question.innerHTML=allQuestions[i].q;
     firstOp.innerHTML=allQuestions[i].a;
     secondOp.innerHTML=allQuestions[i].b;
     thirdOp.innerHTML=allQuestions[i].c;
+
+    firstOp.setAttribute("onclick", "userAnswer("+i+",'a')");
+    secondOp.setAttribute("onclick", "userAnswer("+i+",'b')");
+    thirdOp.setAttribute("onclick", "userAnswer("+i+",'c')");
 }
 
 
 
 // When the button is pressed, the timer begins to count down.
 function countTimer() {
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
         console.log(seconds);
         seconds--;
         timer.innerHTML = "Time Remaining: " + seconds;
         if(seconds < 1) {
-            clearInterval(timeInterval);
+            endTest();
         }
     }, 1000);
 }
 
 // User selections are logged and tracked
-function userAnswer(userAnswer) {
+function userAnswer(questionIndex, userAnswer) {
     console.log(userAnswer);
     console.log(allQuestions[i].answer);
 
-    if(userAnswer === allQuestions[i].answer) {
+    if(userAnswer === allQuestions[questionIndex].answer) {
         console.log("Correct!");
         scorePlus++;
     } else {
@@ -76,17 +89,40 @@ function userAnswer(userAnswer) {
         seconds -= 10;
     }
 
-    i++;
     scoreCount.innerHTML=scorePlus + "/10";
 
-    if(i < allQuestions.length || seconds > 1) {
-        actualTest(i, allQuestions);
+    if (questionIndex === allQuestions.length - 1 || seconds < 1) {
+        endTest();
     } else {
-        clearInterval(timeInterval);
-        quizArea.style.visibility="hidden";
-        initialArea.style.visibility="visible";
+        actualTest(questionIndex + 1)
     }
 }
 
+initialForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var initialsValue = document.getElementById("initials").value;
+    var storedScore = JSON.parse(localStorage.getItem("finalScore"));
+    if (storedScore == null) {
+        storedScore = [];
+    }
+    storedScore.push({
+        initials:initialsValue, score:scorePlus
+    });
 
+    localStorage.setItem("finalScore", JSON.stringify(storedScore));
+
+    printScoreList();
+});
+
+function printScoreList(){
+    allScores.innerHTML = '';
+    var storedScore = JSON.parse(localStorage.getItem("finalScore"));
+    if (storedScore.length > 0) {
+        for (i = 0; i < storedScore.length; i++){
+            var entry = document.createElement('li');
+            entry.textContent = storedScore[i].initials +":" + storedScore[i].score;
+            allScores.appendChild(entry);
+        }
+    }
+}
 
